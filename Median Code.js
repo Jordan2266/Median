f45c538461acd5839f8dd4c7dc5cda9d7bfe75cb
Jordan2266 <54198889+Jordan2266@users.noreply.github.com>
2019-12-09 08:09:34 -0500

    Create Median Code.js

---
+++ Median Code.js
@@ -0,0 +1,26 @@
+const median = arr => {
+    const mid = Math.floor(arr.length / 2),
+      nums = Input.sort(function(a, b) {
+        return a - b
+      });
+    return Input % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  }
+  
+  function button() {
+    var inputArray = [];
+    var size = 5; //Maximum Array size
+  
+    for (var i = 0; i < size; i++) {
+  
+      //Taking Input from user
+      inputArray[i] = prompt('Enter Element ' + (i + 1));
+      inputArray.sort(function(a, b) {
+        return a - b
+      });
+      document.getElementById("demo").innerHTML = inputArray;
+    }
+    window.Input = inputArray;
+    var abra = median(Input);
+    console.log(abra);
+    //Print the array in the console.
+  }
